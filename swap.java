187e977f9d79d47044c3452c178ea7ca33e27f15
Muppu-Snigdha <muppusnigdha@gmail.com>
2025-11-06 16:25:48 +0530

    Refactor swapping class and fix output formatting

--- swap.java
+++ swap.java
@@ -1,10 +1,11 @@
-class swapping{
-public static void main(String args[]){
-int a=16,b=17,temp;
-System.out.println(Before swapping a:"+a+",b:"+b");
-temp=a;
-a=b;
-b=temp;
-System.out.println(After swapping a:"+a+",b:"+b");
+class Swapping {
+    public static void main(String args[]) {
+        int a = 16, b = 17, temp;
+        System.out.println("Before swapping a: " + a + ", b: " + b);
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping a: " + a + ", b: " + b);
+    }
 }
-}
+
